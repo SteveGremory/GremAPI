bd065a436a1f20f7f8d60006142cf073475e17ba
Steve Gremory <38187217+SteveGremory@users.noreply.github.com>
2020-09-29 20:12:02 +0530

    Genesis image update fixed

--- src/Controllers/Controllers.ts
+++ src/Controllers/Controllers.ts
@@ -63,10 +63,8 @@ export const SignUpIPFS = async (req, res) => {
           {
             id: "1",
             text: "Welcome To Grem!",
-            image: {
-              uri:
-                "https://images.pexels.com/photos/4101555/pexels-photo-4101555.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-            },
+            image:
+              "https://images.pexels.com/photos/4101555/pexels-photo-4101555.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
           },
         ],
       })
